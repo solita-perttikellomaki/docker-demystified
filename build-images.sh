0ad3e3b0156e436fb0ee9d130a94a0e00215a80e
Pertti Kellomäki <pertti.kellomaki@solita.fi>
2023-09-04 09:37:46 +0300

    Add shell script for docker image building

---
+++ build-images.sh
@@ -0,0 +1,5 @@
+docker build -f Dockerfile.base -t example-base .
+docker build -f Dockerfile.bash -t example-bash .
+docker build -f Dockerfile.ps -t example-ps .
+docker build -f Dockerfile.ls -t example-ls .
+
